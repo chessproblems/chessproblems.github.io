5da548f51bf4e294ab1220ef49e3d2493b13f8be
chessproblems <predrag.terzic@protonmail.com>
2025-06-05 08:15:55 +0200

    Update config.js

--- src/app/config.js
+++ src/app/config.js
@@ -19,7 +19,7 @@ const configuration = {
 		circlesarrows: true,
 		playAudio: false,
 		playSpeech: false,
-		lichess_userID:'orcan57'
+		lichess_userID:'orcan57',
 	},
 
 	// Feature flags for components
